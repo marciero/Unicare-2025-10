969849a32073b8debb87844276a5ea6b16560e3e
marciero <marciero@une.edu>
2025-10-25 07:47:02 -0400

    update model summary

---
+++ covariates_summary_stats.R
@@ -0,0 +1,23 @@
+#### Proportion of patients with conditions, by cohort
+
+
+patients_events %>% group_by(cohort) %>% 
+  summarize(across(-c(pid, inpatient_count), mean)) %>% 
+  pivot_longer(-cohort, names_to = "condition", values_to = "mean") %>% 
+  filter(condition != "age") %>% 
+  filter(condition != "condition_count") %>% 
+  ggplot(aes(mean, condition,  fill = cohort)) +
+  geom_col(position = "dodge")
+
+
+
+patients_events %>% group_by(cohort) %>% 
+  summarize(across(-pid, mean)) %>% 
+  kbl()
+
+
+## The non Unicare have some zero chronic conditions counts
+
+patients_events %>% group_by(cohort) %>% 
+  summarize(mean(condition_count == 0)) %>% 
+  kbl()
